3f92baefd5d6c45f323853685687378c2adde149
Basavaraj K N <Basavaraj.K@Stackroute.in>
2018-05-08 13:05:28 +0530

    Intermediate commit, able to run few test cases

--- webapp/webpack.common.js
+++ webapp/webpack.common.js
@@ -9,7 +9,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: "babel-loader",
         query: {
-          presets: ['env', "es2015", "react", "stage-2"]
+          presets: ['env', "react", "stage-2"]
         },
       },
       {
--- webapp/webpack.prod.js
+++ webapp/webpack.prod.js
@@ -2,8 +2,6 @@ const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-// const CleanWebpackPlugin = require('clean-webpack-plugin')
-
 
 module.exports = merge(common, {
   devtool: 'source-map',
